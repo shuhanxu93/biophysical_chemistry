f34d848cb9b39dd2d8138f570f670e99775731ec
shuhanxu93 <shuhan.xu@stud.ki.se>
2018-04-19 16:51:06 +0200

    added file

---
+++ lab7/lab7_files/plot_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import numpy as np 
+import pylab as plt
+
+
+hists = np.genfromtxt('histo_2.xvg', skip_header=12)
+profile = np.genfromtxt('profile_2.xvg', skip_header=12)
+
+
+N = np.shape(hists)[1] - 1 
+x = hists[:,0]
+
+f = plt.figure()
+
+ax1=f.add_subplot(211)
+for i in np.arange(N):
+	ax1.plot(hists[:,i+1])
+
+ax2=f.add_subplot(212)
+ax2.plot(profile[:,0],profile[:,1])
+plt.show()
